ea8432971888461085a635d092a02f2dffb0974e
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-22 12:50:04 +0530

    js1.js modified

--- p3_js1.js
+++ p3_js1.js
@@ -1,11 +1,12 @@
 // 
-
-function book(name,author,type){
+class Book{
+constructor(name,author,type){
  this.name=name;
  this.book=book;
  this.type=type;
 }
-function display(){
+}
+class Display{
     added(book){
        let target=document.getElementById('tableBody');
        let str= `<tr>
@@ -25,7 +26,26 @@ let libraryForm=document.getElementById('libraryForm');
 libraryForm.addEventListener('submit',formsubmit);
 
 function formsubmit(e){
-      
+      let name=document.getElementById('bookName').value;
+      let author=document.getElementById('author').value;
+      let type;
+      let fiction=document.getElementById('fiction');
+      let programming=document.getElementById('programming');
+      let cooking=document.getElementById('cooking');
+      if(fiction.clicked){
+          type=fiction.value;
+      }
+      else if(programming.clicked){
+          type=programming.value;
+      }
+      else if(cooking.clicked){
+          type=cooking.value;
+      }
+      let book=new Book(name,author,type);
+      let display=new Display();
+      display.added(book);
+      display.cleared();
+
       e.preventDefault();    // everytime we submit the form it 
                             // get refresh to prevent that we can use preventDefault();
     
