6637284066a77671b6956dade3b2fe0ce360d7b4
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-22 12:38:19 +0530

    p3

--- p3_js1.js
+++ p3_js1.js
@@ -1,10 +1,33 @@
 // 
+
+function book(name,author,type){
+ this.name=name;
+ this.book=book;
+ this.type=type;
+}
+function display(){
+    added(book){
+       let target=document.getElementById('tableBody');
+       let str= `<tr>
+       <td>${book.name}</td>
+       <td>${book.author}</td>
+       <td>${book.type}</td> 
+       </tr> `;
+       target.innerHTML+=str;
+    }
+
+    cleared(){
+        doc=document.getElementById('libraryForm');
+        doc.reset();
+    }
+}
 let libraryForm=document.getElementById('libraryForm');
 libraryForm.addEventListener('submit',formsubmit);
 
 function formsubmit(e){
+      
       e.preventDefault();    // everytime we submit the form it 
                             // get refresh to prevent that we can use preventDefault();
-    console.log('done bro');
+    
     
 }
