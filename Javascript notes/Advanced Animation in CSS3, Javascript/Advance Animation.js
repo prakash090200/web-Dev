02b2a2f755a5022f45f92b5631c40daffbef5186
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-12-01 18:03:36 +0530

    advance animation

---
+++ Javascript notes/Advanced Animation in CSS3, Javascript/Advance Animation.js
@@ -0,0 +1,9 @@
+    var audio=document.createElement('audio');
+    audio.setAttribute('src','police.mp3');
+    audio.loop=true;
+    audio.play();
+
+    var audio=document.createElement('audio');
+    audio.setAttribute('src','jungle5.mp3');
+    audio.loop=true;
+    audio.play();
